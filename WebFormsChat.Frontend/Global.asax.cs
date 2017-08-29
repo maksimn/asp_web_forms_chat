f10a7573f79170a9a3a994a108ef51cacb86bb27
maksimn <maksim1989sovunion@yandex.ru>
2017-08-29 13:07:07 +0300

    App routes

--- WebFormsChat.Frontend/Global.asax.cs
+++ WebFormsChat.Frontend/Global.asax.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.Practices.Unity;
 using System;
 using System.Web;
+using System.Web.Routing;
 
 namespace WebFormsChat.Frontend {
     public class Global : HttpApplication {
         protected void Application_Start(object sender, EventArgs e) {
+            RegisterRoutes(RouteTable.Routes);
             UnityConfig.RegisterTypes();
+        }
+
+        private static void RegisterRoutes(RouteCollection routes) {
+            routes.MapPageRoute("Register", "Pages/Register.aspx", "~/Pages/Register.aspx");
+            routes.MapPageRoute("Login", "Pages/Login.aspx", "~/Pages/Login.aspx");
+            routes.MapPageRoute("Any", "{*url}", "~/Pages/Chat.aspx");
         }
 
         protected void Application_PreRequestHandlerExecute(object sender, EventArgs e) {
