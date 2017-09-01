1d78ce717b91778441e93cf57990a13067c8782f
maksimn <maksim1989sovunion@yandex.ru>
2017-09-01 21:11:56 +0300

    WebFormsChat Database definition

---
+++ WebFormsChat.ChatData/WebFormsChat.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE [WebFormsChat]
+ON
+( 
+   NAME = 'WebFormsChat', 
+   FILENAME = 'C:\Users\MSSQLSERVER\DataBases\WebFormsChat.mdf', 
+   SIZE = 5120KB , MAXSIZE = 10240KB , FILEGROWTH = 1024KB 
+)
+LOG ON 
+( 
+   NAME = 'WebFormsChat_log', 
+   FILENAME = 'C:\Users\MSSQLSERVER\DataBases\WebFormsChat_db.ldf', 
+   SIZE = 1024KB , MAXSIZE = 10240KB , FILEGROWTH = 1024KB 
+);
+
+GO
+
+USE WebFormsChat;
+
+GO
+
+CREATE TABLE Users (
+   Id int NOT NULL PRIMARY KEY IDENTITY(1,1),
+   Name nvarchar(50) UNIQUE NOT NULL,
+   PasswordHash nvarchar(1000) NOT NULL
+); 
+
+CREATE TABLE ChatMessages (
+   Id int NOT NULL PRIMARY KEY IDENTITY(1,1),
+   UserId int NOT NULL FOREIGN KEY REFERENCES Users(Id),
+   Text nvarchar(500) NOT NULL
+);
